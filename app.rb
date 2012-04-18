661e10eea6f4898f9907ab9ad10eeb5362061721
Prabhu <prabhu.saitu@gmail.com>
2012-04-18 22:57:28 +0530

    changes

--- app.rb
+++ app.rb
@@ -296,7 +296,7 @@ end
 get '/make' do
   if session[:admin] 
     
-    @students = Student.all.sort(:regno.desc)
+    @students = Student.sort(:regno.desc)
 
     Spreadsheet.client_encoding = 'UTF-8'
     book = Spreadsheet::Workbook.new
