031c254b1d2aa129ed2b4162e32b9a90ff247977
Prabhu <prabhu.saitu@gmail.com>
2012-04-18 23:11:41 +0530

    changes

--- app.rb
+++ app.rb
@@ -317,7 +317,7 @@ get '/make' do
     @students.each do |s|
         
     
-    if s.regno > 1040000000 && regno < 1041000000
+    if s.regno > 1040000000 && s.regno < 1041000000
       
       sheet1[i, 0] = s.name
       sheet1[i, 1] = s.regno
